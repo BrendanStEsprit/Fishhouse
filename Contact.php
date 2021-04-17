5578e3fec5df2cc074d939c42206e8ce1e113099
BrendanStEsprit <77287738+BrendanStEsprit@users.noreply.github.com>
2021-04-17 12:06:33 -0400

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <meta charset="utf-8" >
+  <title>Thank you for your order</title>
+  <link rel="stylesheet" href="House_Style_Sheet.css">
+</head>
+
+<body>
+  <?php if (isset($_POST['toppings']) && !is_array($_POST['toppings'])) {
+    $toppings_problem = TRUE;
+    $toppings = is_string($_POST['toppings']) ? $_POST['toppings'] : '<em>empty</em>';
+  }
+  else {
+    $toppings_problem = FALSE;
+    $toppings = isset($_POST['toppings']) ? implode(', ', $_POST['toppings']) : '<em>empty</em>';
+  } ?>
+<h1>THANK YOU</h1>
+
+<p>Thank you for ordering from Black Goose Bistro. We have received the following information about your order:</p>
+
+<h2>Your Information</h2>
+<ul>
+<li><strong>Name:</strong> <?php print $_POST['customername'] ? $_POST['customername'] : '<em>empty</em>'; ?></li>
+<li><strong>Address:</strong> <?php print $_POST['address'] ? $_POST['address'] : '<em>empty</em>'; ?></li>
+</ul>
+<p><strong>Delivery instructions:</strong> <?php print $_POST['instructions'] ? $_POST['instructions'] : '<em>empty</em>'; ?></p>
+
+<h2>Your pizza</h2>
+
+<?php if (!isset($_POST['crust']) && !isset($_POST['toppings']) && !isset($_POST['pizzas'])) { ?>
+<em>Sorry, we did not receive your information. <a href="Contact.html">Try again.</a></em>
+<?php } 
+  else { ?>
+    <ul>
+    <li><strong>Crust:</strong> <?php print isset($_POST['crust']) && $_POST['crust'] ? $_POST['crust'] : '<em>empty</em>'; ?></li>
+    <li><strong>Toppings:</strong> <?php
+      print $toppings;
+      if ($toppings_problem) { ?>
+        <span style="color:red">*</span>
+     <?php } ?></li>
+    <li><strong>Number:</strong> <?php print isset($_POST['pizzas']) && $_POST['pizzas'] ? $_POST['pizzas'] : '<em>empty</em>'; ?></li>
+    </ul>
+<?php  }
+if ($toppings_problem) { ?>
+  <hr />
+  <p>&nbsp;</p>
+<?php } ?>
+
+<p class="disclaimer"><small>This site is for educational purposes only. No pizzas will show up at your door.</small></p>
+
+</body>
+</html>
